25282496120470116b41396a3e85ed800ee47872
Jorge <jorgesifontes.cl@gmail.com>
2021-02-04 22:10:26 -0300

    Create Home Controller, Adding Navbar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    format.html { redirect_to root_path, alert: 'Welcome' }
+  end
 
   # GET /resource/edit
   # def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'temp/index'
-  root to: 'temp#index'
+  
+  root to: 'home#index'
 end
