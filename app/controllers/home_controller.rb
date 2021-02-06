2570ee82c4ac1a8e53e18ce254cf011a04a627da
Jorge <jorgesifontes.cl@gmail.com>
2021-02-06 20:22:05 -0300

    Adding Modal to tweets new view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
     def index
         @tweets = Tweet.order(created_at: :desc).page(params[:page]) #Se ordena en forma descendiente
-        @tweet = Tweet.new
         @likes = Like.all
+
+        if current_user
+            @tweet = Tweet.new
+        end
     end
 end
