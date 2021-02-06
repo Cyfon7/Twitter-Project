875a8e85568a0ac237d20f53a64c775750cecd6e
Jorge <jorgesifontes.cl@gmail.com>
2021-02-05 23:33:14 -0300

    Adding Pagination & footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
     def index
-        @tweets = Tweet.order(created_at: :desc) #Se ordena en forma descendiente
+        @tweets = Tweet.order(created_at: :desc).page(params[:page]) #Se ordena en forma descendiente
         @likes = Like.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  root to: 'home#index'
+  root "home#index"
+  get '/home', to: "home#index", as: "home"
+
 end
