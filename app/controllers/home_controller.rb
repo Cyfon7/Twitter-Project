b36385e024b9b2dd817e69d9ecd5526bae0b36a3
Jorge <jorgesifontes.cl@gmail.com>
2021-02-07 15:24:51 -0300

    Fixing issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,6 @@ class HomeController < ApplicationController
         @tweets = Tweet.order(created_at: :desc).page(params[:page]) #Se ordena en forma descendiente
         if current_user
             @tweet = Tweet.new
-            if params[:rt_id]
-                @rt_id = params[:rt_id]
-            end
         end
     end
 end
