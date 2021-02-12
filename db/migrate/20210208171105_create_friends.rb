3a595900e1eb2a0b67f05252bd6f953192db8ef8
Jorge <jorgesifontes.cl@gmail.com>
2021-02-11 22:55:10 -0300

    Rewriting 20210208171105 migration & erasing 2021021001004 for heroku

--- db/migrate/20210208171105_create_friends.rb
+++ db/migrate/20210208171105_create_friends.rb
@@ -2,7 +2,7 @@ class CreateFriends < ActiveRecord::Migration[5.2]
   def change
     create_table :friends do |t|
       t.references :user, foreign_key: true
-      t.references :friend
+      t.integer :friend_id
       t.timestamps
     end
   end
--- db/migrate/20210210010004_remove_recursive_friend_reference.rb
+++
@@ -1,6 +0,0 @@
-class RemoveRecursiveFriendReference < ActiveRecord::Migration[5.2]
-  def change
-    remove_foreign_key :friends, :friend_id
-    remove_index :friends, name: "index_friends_on_friend_id"
-  end
-end
