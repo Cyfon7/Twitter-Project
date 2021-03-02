bd2bcd1fe646030dd4ae061e1173f2cbca1f1d65
Jorge <jorgesifontes.cl@gmail.com>
2021-03-01 21:20:44 -0300

    model details

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
     has_many :retweets, class_name: "Tweet", foreign_key: "tweet_id"
     belongs_to :tweet, class_name: "Tweet", optional: true
     belongs_to :user
-    has_many :likes#, as: :publication
+    has_many :likes, dependent: :destroy#, as: :publication
     
     validates :content, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :likes#, as: :publication
-  has_many :tweets
+  has_many :likes, dependent: :destroy #, as: :publication
+  has_many :tweets, dependent: :destroy
 
   validates :name, :email, :image, presence: true
 
